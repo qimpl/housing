57bf8ff7d2492c1f53afd73766331007d974c4ed
Baraffe Robin <robin.baraffe@gmail.com>
2020-11-24 16:41:21 +0100

    Split middleware function into middlewares.go file (#31)

--- router/router.go
+++ router/router.go
@@ -1,15 +1,10 @@
 package router
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/qimpl/housing/models"
-	"github.com/qimpl/housing/services"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
@@ -26,7 +21,7 @@ func CreateRouter() {
 	APIRouter.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
 	if os.Getenv("ENV") != "dev" {
-		router.Use(jwtVerifyTokenMiddleware)
+		router.Use(jwtTokenVerificationMiddleware)
 	}
 
 	createHousingRouter(APIRouter)
@@ -53,38 +48,4 @@ func CreateRouter() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), handler); err != nil {
 		log.Print(err)
 	}
-}
-
-func jwtVerifyTokenMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		requestURI := strings.TrimPrefix(r.RequestURI, "/api/v1")
-		for _, route := range unProtectedRoutes {
-			if strings.Contains(requestURI, route) {
-				next.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer")
-		if len(authHeader) != 2 {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			var badRequest *models.BadRequest
-			json.NewEncoder(w).Encode(badRequest.GetError("Malformed Authorization HTTP header"))
-
-			return
-		}
-
-		if _, err := services.ValidateJwtToken(authHeader[1]); err != nil {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusUnauthorized)
-			var Unauthorized *models.Unauthorized
-			json.NewEncoder(w).Encode(Unauthorized.GetError(fmt.Sprintf("Invalid jwt token : %s", err)))
-
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
