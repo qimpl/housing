bceeb2c2a1193e17505455ca4c02a34a8856706c
Jérôme Pogeant <p-jerome@hotmail.fr>
2021-05-14 17:16:06 +0200

    feat(notifications): send email when a new visit is requested of accepted (#42)

--- go.mod
+++ go.mod
@@ -5,25 +5,26 @@ go 1.15
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/go-openapi/spec v0.19.9 // indirect
-	github.com/go-openapi/swag v0.19.9 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/go-pg/pg/v10 v10.4.0
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
-	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mailgun/mailgun-go/v4 v4.5.1
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/minio/minio-go/v7 v7.0.10
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/cors v1.7.0
 	github.com/swaggo/http-swagger v0.0.0-20200308142732-58ac5e232fba
-	github.com/swaggo/swag v1.6.7
+	github.com/swaggo/swag v1.7.0
 	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/crypto v0.0.0-20210503195802-e9a32991a82e // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/net v0.0.0-20210505024714-0287a6fb4125 // indirect
-	golang.org/x/sys v0.0.0-20210503173754-0981d6026fa6 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
 	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-	golang.org/x/tools v0.1.0 // indirect
+	golang.org/x/tools v0.1.1 // indirect
 	googlemaps.github.io/maps v1.3.1
 )
--- handlers/visit.go
+++ handlers/visit.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/qimpl/housing/db"
 	"github.com/qimpl/housing/models"
+	"github.com/qimpl/housing/services"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -26,9 +28,9 @@ import (
 // @Router /visit [post]
 func CreateVisit(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var visit *models.Visit
+	var body *models.CreateVisitBody
 
-	if err := json.NewDecoder(r.Body).Decode(&visit); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		var unprocessableEntity *models.UnprocessableEntity
 		json.NewEncoder(w).Encode(unprocessableEntity.GetError("Body malformed data"))
@@ -37,7 +39,14 @@ func CreateVisit(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	visit, err := db.CreateVisit(visit)
+	var err error
+	visit := &models.Visit{
+		Date:      body.Date,
+		Hour:      body.Hour,
+		HousingID: body.HousingID,
+	}
+
+	visit, err = db.CreateVisit(visit)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		var badRequest *models.BadRequest
@@ -46,6 +55,20 @@ func CreateVisit(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
+
+	services.SendEmail(
+		fmt.Sprintf("Qimpl | %s veut visiter votre bien ! 🎉", body.Owner.FirstName),
+		"",
+		body.Owner.Email,
+		"visit_reservation",
+		&services.TemplateValues{
+			IsEng: false,
+			Variables: map[string]string{
+				"first_name": body.Owner.FirstName,
+				"visit_link": fmt.Sprintf("https://qimpl.io/visit/%s", visit.ID),
+			},
+		},
+	)
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(visit)
@@ -93,14 +116,27 @@ func GetVisitByHousingID(w http.ResponseWriter, r *http.Request) {
 // @Tags Visits
 // @Produce json
 // @Param visit_id path string true "Visit booking ID"
+// @Param visit body models.AcceptVisit true "Accept Visit data"
 // @Success 204 ""
 // @Failure 400 {object} models.ErrorResponse
 // @Failure 404 {object} models.ErrorResponse
 // @Router /visit/{visit_id}/accept [patch]
 func AcceptVisit(w http.ResponseWriter, r *http.Request) {
-	visitID := uuid.MustParse(mux.Vars(r)["visit_id"])
+	var body *models.AcceptVisit
 
-	if _, err := db.GetVisitBookingByID(visitID); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		var unprocessableEntity *models.UnprocessableEntity
+		json.NewEncoder(w).Encode(unprocessableEntity.GetError("Body malformed data"))
+		log.Printf("Visit - Accept - Body Error - %s : %s\n", time.Now(), err)
+
+		return
+	}
+
+	visitID := uuid.MustParse(mux.Vars(r)["visit_id"])
+	visit, err := db.GetVisitBookingByID(visitID)
+
+	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusNotFound)
 		var notFound *models.NotFound
@@ -119,6 +155,22 @@ func AcceptVisit(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
+
+	visitDate := fmt.Sprintf("%s %s", visit.Date.Format("02-Jan-2006"), visit.Hour)
+
+	services.SendEmail(
+		fmt.Sprintf("Qimpl | Votre demande de visite du %s a été acceptée ! 🔥", visitDate),
+		"",
+		body.UserEmail,
+		"visit_accepted",
+		&services.TemplateValues{
+			IsEng: false,
+			Variables: map[string]string{
+				"visit_date": visitDate,
+				"visit_link": fmt.Sprintf("https://qimpl.io/visit/%s", visitID),
+			},
+		},
+	)
 
 	w.WriteHeader(http.StatusNoContent)
 }
